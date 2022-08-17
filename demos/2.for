179a273573c0a673cd892d8999fa525c540f4997
Haoning <19302010042@fudan.edu.cn>
2022-08-17 13:05:48 +0800

    fix when forslice appears as leftvalue, add new function assign_forslice(...) to modify part of *original* array

--- demos/2.for
+++ demos/2.for
@@ -1,5 +1,11 @@
-program main
+program arrayReshape
     implicit none
-    integer,dimension(3,4),parameter::c(3,4) = reshape( (/ 1,2,3,4,5,6,7,8,9,10,11,12/), (/ 3, 4 /) )
-    !   const farray<int> c {{1,1},{3,4},forreshape({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, {3, 4})};
-end program
+    integer i
+    integer,parameter::one=1,zero=0
+    integer,dimension(3,12)::a
+    do i=1,3
+        a(i,1:9) = (/one,zero,zero, zero,one,zero, zero,zero,one/)
+        ! assign_forslice(a, make_init_list({one,zero,zero, zero,one,zero, zero,zero,one}),{{i},{1,9}});
+    enddo
+    write(*,*) a(1,1)
+end program arrayReshape
